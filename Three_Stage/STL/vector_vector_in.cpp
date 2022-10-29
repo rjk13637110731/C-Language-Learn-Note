3f3cd46b20adde3c3d117682574e8714a308aba3
rjk13637110731 <86515447+rjk13637110731@users.noreply.github.com>
2022-10-29 22:33:57 +0800

    Create vector_vector_in.cpp

---
+++ Three_Stage/STL/vector_vector_in.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//容器嵌套容器
+
+void test01() {
+	vector<vector<int>> v;
+
+	vector<int> v1;
+	vector<int> v2;
+	vector<int> v3;
+	vector<int> v4;
+
+	//向容器中添加数据
+	for (int i = 0; i < 4;i++) {
+		v1.push_back(i + 1);
+		v2.push_back(i + 2);
+		v3.push_back(i + 3);
+		v4.push_back(i + 4);
+	}
+
+	//将小容器插入到大容器中
+	v.push_back(v1);
+	v.push_back(v2);
+	v.push_back(v3);
+	v.push_back(v4);
+
+	//通过大容器把所有的数据遍历一遍
+	for (vector<vector<int>>::iterator iti = v.begin(); iti != v.end();iti++) {//(*iti)就是一个容器---vector<int>
+		for (vector<int>::iterator itj = (*iti).begin(); itj != (*iti).end(); itj++) {
+			cout << (*itj) << "  ";
+		}
+		cout << endl;
+	}
+}
+
+
+int main() {
+
+	test01();
+
+	return 0;
+}
