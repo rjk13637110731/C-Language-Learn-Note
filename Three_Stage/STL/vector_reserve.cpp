08c04de8295f9a344733bdbd6a98f8cdea007574
rjk13637110731 <86515447+rjk13637110731@users.noreply.github.com>
2022-10-30 15:39:44 +0800

    Create vector_reserve.cpp

---
+++ Three_Stage/STL/vector_reserve.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//vector预留空间
+
+void test01() {
+	vector<int>v;
+	
+	//利用reserve预留空间
+	v.reserve(100000);
+
+	int num = 0;//统计开辟的次数
+	int* p = NULL;
+	for (int i = 0; i < 100000;i++) {
+		v.push_back(i);
+
+		if (p != &v[0]) {
+			p = &v[0];
+			num++;
+		}
+	}
+	cout << "num = " << num << endl;
+
+
+
+}
+
+int main() {
+
+	test01();
+
+	return 0;
+}
