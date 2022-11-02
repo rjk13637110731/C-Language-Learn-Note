f887ce2d2cbf990bfbb2ac2812d2eef72c495de3
rjk13637110731 <86515447+rjk13637110731@users.noreply.github.com>
2022-11-02 20:57:27 +0800

    Create map_constructor.cpp

---
+++ Three_Stage/STL/map_constructor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+//map容器的构造与赋值
+
+void printMap(map<int,int>&m) {
+	for (map<int, int>::iterator it = m.begin(); it != m.end();it++) {
+		cout << "key = " << (*it).first << "\tvalue = " << it->second << endl;
+	}
+	cout << endl;
+}
+
+void test01() {
+	map<int, int>m;
+	m.insert(pair<int, int>(4, 40));
+	m.insert(pair<int, int>(1, 10));
+	m.insert(pair<int, int>(3, 30));
+	m.insert(pair<int, int>(2, 20));
+	printMap(m);
+
+	//拷贝构造
+	map<int, int>m2(m);
+	printMap(m2);
+
+	//赋值
+	map<int, int>m3;
+	m3 = m2;
+	printMap(m3);
+}
+
+int main() {
+	
+	test01();
+
+	return 0;
+}
