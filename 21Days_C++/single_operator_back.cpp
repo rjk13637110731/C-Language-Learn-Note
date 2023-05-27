f8864ea78fd85bf32ddde7c8f0cb2b10453065a0
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-05-27 10:49:45 +0800

    Add files via upload

---
+++ 21Days_C++/single_operator_back.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class point {
+private:
+	int x;
+	int y;
+public:
+	point(int xx = 0, int yy = 0) {
+		x = xx;
+		y = yy;
+	}
+	int getx() {
+		return x;
+	}
+	int gety() {
+		return y;
+	}
+	point operator++();
+	point operator--(int);
+};
+
+point point::operator++() {
+	++x;
+	++y;
+	return *this;
+}
+point point::operator--(int) {
+	x--;
+	y--;
+	return *this;
+}
+
+int main() {
+
+	point ob(3, 4);
+	cout << "ob.x = " << ob.getx() << "    ob.y = " << ob.gety() << endl;
+	++ob;
+	cout << "ob.x = " << ob.getx() << "    ob.y = " << ob.gety() << endl;
+	ob.operator--(0);
+	cout << "ob.x = " << ob.getx() << "    ob.y = " << ob.gety() << endl;
+	return 0;
+}
