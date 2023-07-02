282e69d2144f7af786428637292c879e1c56b8ed
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-02 16:56:15 +0800

    Create namespace.cpp

---
+++ 21Days_C++/namespace.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+# include <fstream>
+# include <string>
+# include "out.h"
+
+using namespace std;
+
+int main() {
+	
+	ofstream outf("test.txt");
+
+	if (!outf) {
+		cout << "不能打开输出文件！" << endl;
+		return 1;
+	}
+
+	double n = 123.456;
+	char str[] = "向文件中写入双精度数和字符串！";
+	outf.write((char*)&n, sizeof(double));
+	outf.write(str,strlen(str));
+	outf.close();
+
+	ifstream inf("test.txt");
+	if (!inf){
+		cout << "不能打开输入文件！" << endl;
+		return 1;
+	}
+	inf.read((char*)&n, sizeof(double));
+	inf.read(str, 26);
+	cout << n << "     " << str << endl;
+	inf.close();
+
+	Outer::i = 0;
+	Outer::f();
+	Outer::Inner::i == 0;
+	Outer::Inner::g();
+	Outer::Inner::h();
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Outer::i = " << Outer::i << ", Inner::i = " << Outer::Inner::i << std::endl;
+
+	return 0;
+}
