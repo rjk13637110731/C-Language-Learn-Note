2c280cf6ebb25d374dda2de6807be6e810647320
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-08 20:15:55 +0800

    Create string_STL_2.cpp

---
+++ 21Days_C++/string_STL_2.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+void main() {
+	string str1 = "hello";
+	string str2 = "Hello";
+	string str3 = "Hello World";
+
+	int flag;
+
+	flag = (str1 == str2);
+	if (flag == 1) {
+		cout << "str1 == str2" << endl;
+	}
+	else {
+		flag = (str1 > str2);
+		if (flag == 1) {
+			cout << "str1 > str2" << endl;
+		}
+		else {
+			cout << "str1 < str2" << endl;
+		}
+	}
+
+	flag = (str2 == str3);
+	if (flag == 1) {
+		cout << "str2 == str3" << endl;
+	}
+	else {
+		flag = (str2 > str3);
+		if (flag == 1) {
+			cout << "str2 > str3" << endl;
+		}
+		else {
+			cout << "str2 < str3" << endl;
+		}
+	}
+}
