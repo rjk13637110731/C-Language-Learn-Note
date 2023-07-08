9f453969e6ba23953492de9c17ea6e2883d3ae25
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-08 20:28:08 +0800

    Create string_STK_4.cpp

---
+++ 21Days_C++/string_STK_4.cpp
@@ -0,0 +1,59 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+void main() {
+	string str1 = "hello";
+	string str2 = "Hello";
+	string str3 = "Hello World";
+
+	int flag;
+
+	flag = (str1 == str2);
+	if (flag == 1) {
+		cout << "str1 == str2" << endl;
+	}
+	else {
+		flag = (str1 > str2);
+		if (flag == 1) {
+			cout << "str1 > str2" << endl;
+		}
+		else {
+			cout << "str1 < str2" << endl;
+		}
+	}
+
+	flag = (str2 == str3);
+	if (flag == 1) {
+		cout << "str2 == str3" << endl;
+	}
+	else {
+		flag = (str2 > str3);
+		if (flag == 1) {
+			cout << "str2 > str3" << endl;
+		}
+		else {
+			cout << "str2 < str3" << endl;
+		}
+	}
+
+	string str4 = ("Welcome to 21 C++");
+	int num;
+	num = str4.size();
+	cout << "The Length of  " << str4 << " is : " << num << endl;
+
+	if (num == 0) {
+		cout << "The str4 is empty" << endl;
+	}
+	else {
+		cout << "The str4 is not empty" << endl;
+	}
+
+	if (str4.empty()) {
+		cout << "The str4 is empty" << endl;
+	}
+	else {
+		cout << "The str4 is not empty" << endl;
+	}
+}
