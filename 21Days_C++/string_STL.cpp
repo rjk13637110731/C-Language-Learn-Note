98f8cdf2993236686a7bdc0b48258c13c8b90edd
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-08 20:02:24 +0800

    Create string_STL.cpp

---
+++ 21Days_C++/string_STL.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main() {
+	
+	string str;
+	cout << "please input string: " << endl;
+
+	cin >> str;
+	cout << "The string is: " << endl;
+
+	cout << str << endl;
+
+	return 0;
+}
