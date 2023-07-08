95d28636971330afc4695d44ee7f0ed6a2d946a8
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-08 10:26:37 +0800

    Create STL_set.cpp

---
+++ 21Days_C++/STL_set.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+# include <set>
+# include <string>
+
+using namespace std;
+
+int main() {
+	
+	set<string> strset;
+	set<string>::iterator si;
+
+	strset.insert("cantaloupes");
+	strset.insert("apple");
+	strset.insert("orange");
+	strset.insert("banana");
+	strset.insert("grapes");
+	strset.insert("grapes");
+
+	for (si = strset.begin(); si != strset.end();si++) {
+		cout << *si << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
