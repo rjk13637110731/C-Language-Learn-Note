a18704f426b053e405dd0d29fb2f68473b55684f
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-12 00:37:57 +0800

    Create 3.10.cpp

---
+++ C++程序设计/chapter3/3.10.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int i = 1;
+	int sum = 0;
+
+	while (i <= 100) {
+		sum += i;
+		i++;
+	}
+
+	cout << "sum = " << sum << endl;
+
+	return 0;
+}
