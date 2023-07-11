299fae34ac4a11350af5162212d88708c2d93617
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-11 14:44:25 +0800

    Create 2.3.cpp

---
+++ C++程序设计/chapter2/2.3.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# define PRICE 30
+using namespace std;
+
+int main() {
+	
+	int num, total;
+	num = 10;
+	total = num * PRICE;
+	cout << "Total = " << total << endl;
+
+	return 0;
+}
