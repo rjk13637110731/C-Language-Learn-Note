c833213f89d5c1deb3657f955fddce0af58ccaf6
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-12 01:38:03 +0800

    Create 4.1.cpp

---
+++ C++程序设计/chapter4/4.1.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+
+using namespace std;
+
+void printstart(void) {
+	cout << "***************************" << endl;
+}
+
+void print_message(void) {
+	cout << "Welcome to C++!" << endl;
+}
+
+int main() {
+	
+	printstart();
+	print_message();
+	printstart();
+
+	return 0;
+}
