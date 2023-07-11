cf978c84f985527820252a44c8083d6f5e6aec5f
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-11 14:41:44 +0800

    Create 2.2-new.cpp

---
+++ C++程序设计/chapter2/2.2-new.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	char c1, c2;
+	c1 = 'a';
+	c2 = 'b';
+	c1 = c1 - 32;
+	c2 = c2 - 32;
+	cout << c1 << ' ' << c2 << '\n';
+
+	cout << "We are friends\
+	all right" << endl;
+
+	return 0;
+}
