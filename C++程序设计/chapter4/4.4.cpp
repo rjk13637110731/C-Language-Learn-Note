f3f94cc12cb212e53ee01fcca86e7ff070415cf9
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-12 20:06:02 +0800

    Create 4.4.cpp

---
+++ C++程序设计/chapter4/4.4.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+inline int max(int,int,int);
+
+int main() {
+
+	int i = 10, j = 20, k = 30, m;
+
+	m = max(i, j, k);
+	cout << "Max = " << m << endl;
+
+	return 0;
+}
+
+inline int max(int a,int b,int c) {
+	if (b > a) {
+		a = b;
+	}
+	if (c > a) {
+		a = c;
+	}
+
+	return a;
+}
