0756bf86947e2a662955a3708aba6d764e41be12
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-12 20:20:59 +0800

    Create 4.6.cpp

---
+++ C++程序设计/chapter4/4.6.cpp
@@ -0,0 +1,33 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int max(int a, int b, int c);
+	int max(int a, int b);
+
+	int a = 8, b = -12, c = 27;
+	cout << "max(a,b,c)" << max(a, b, c) << endl;
+	cout << "max(a,b)" << max(a, b) << endl;
+
+	return 0;
+}
+
+int max(int a,int b,int c) {
+	if (b > a) {
+		a = b;
+	}
+	if (c > a) {
+		a = c;
+	}
+	return a;
+}
+
+int max(int a, int b) {
+	if (b > a) {
+		a = b;
+	}
+
+	return a;
+}
