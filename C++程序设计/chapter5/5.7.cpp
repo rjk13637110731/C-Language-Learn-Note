90ff110f5047fe1f4fe4b66446f5ba0fb406d020
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-14 14:53:12 +0800

    Create 5.7.cpp

---
+++ C++程序设计/chapter5/5.7.cpp
@@ -0,0 +1,38 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	void select_sort(int array[],int n);
+	int a[10], i;
+	cout << "Enter the original array:" << endl;
+	for (i = 0; i < 10;i++) {
+		cin >> a[i];
+	}
+
+	cout << endl;
+
+	select_sort(a, 10);
+	cout << "The sorted array:" << endl;
+	for (i = 0; i < 10;i++) {
+		cout << a[i] << " ";
+	}
+
+	cout << endl;
+
+	return 0;
+}
+
+void select_sort(int array[],int n) {
+	int i, j, k, t;
+	for (i = 0; i < n - 1;i++) {
+		for (j = i + 1; j < n;j++) {
+			if (array[j] < array[i]) {
+				t = array[i];
+				array[i] = array[j];
+				array[j] = t;
+			}
+		}
+	}
+}
