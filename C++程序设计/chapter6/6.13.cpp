d22ba258e681cdff3f000e531ef54e3c96c7ce68
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-22 16:36:53 +0800

    Create 6.13.cpp

---
+++ C++程序设计/chapter6/6.13.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+
+using namespace std;
+
+int main() { 
+
+	char str1[] = "I Love China!", str2[20], * p1, * p2;
+	p1 = str1;
+	p2 = str2;
+	for (; *p1 != '\0';p1++,p2++) {
+		*p2 = *p1;
+	}
+	*p2 = '\0';
+	p1 = str1;
+	p2 = str2;
+	cout << "str1 is: " << str1 << endl;
+	cout << "str2 is: " << str2 << endl;
+
+	return 0;
+}
