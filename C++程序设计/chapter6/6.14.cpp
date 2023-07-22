f1a82b3dd5dc3c970a2f37905b3b5013535c0bd3
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-22 16:44:17 +0800

    Create 6.14.cpp

---
+++ C++程序设计/chapter6/6.14.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int max(int ,int);
+	int a, b, m;
+	cin >> a >> b;
+	m = max(a, b);
+	cout << "max = " << m << endl;
+
+	return 0;
+}
+
+int max(int x,int y) {
+	int z;
+	if (x > y) {
+		z = x;
+	}
+	else {
+		z = y;
+	}
+
+	return z;
+}
