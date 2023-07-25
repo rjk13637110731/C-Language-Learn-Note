10bafeb6604dcb58d9b21d18833a735c5ec66246
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-26 00:56:26 +0800

    Create 6.19.cpp

---
+++ C++程序设计/chapter6/6.19.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	void swap(int*, int*);
+	int a = 10;
+	int b = 20;
+	cout << "交换之前：" << endl;
+	cout << a << "  " << b << endl;
+	swap(&a, &b);
+	cout << "交换之后：" << endl;
+	cout << a << "  " << b << endl;
+
+	return 0;
+}
+
+void swap(int *a,int *b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
