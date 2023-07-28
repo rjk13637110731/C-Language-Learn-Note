c1d37e4fa6a4b24e0b83074d51a02b8fe4aa2682
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-28 10:50:08 +0800

    Create 7.5-3.cpp

---
+++ C++程序设计/chapter7/7.5-3.cpp
@@ -0,0 +1,30 @@
+// 引用传递结构类型数据
+# include <iostream>
+# include <string>
+using namespace std;
+
+struct Student {
+	int num;
+	string name;
+	float score[3];
+};
+
+int main() {
+	
+	void print(Student &stu);
+
+	Student stu;
+	stu.num = 12345;
+	stu.name = "Li Fung";
+	stu.score[0] = 67.5;
+	stu.score[1] = 89;
+	stu.score[2] = 78.5;
+	print(stu);
+
+	return 0;
+}
+
+
+void print(Student &stu) {
+	cout << stu.num << " " << stu.name << " " << stu.score[0] << " " << stu.score[1] << " " << stu.score[2] << endl;
+}
