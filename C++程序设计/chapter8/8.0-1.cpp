3783ebea68fd5e5229818732972af1f6bfae5c13
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-29 21:17:27 +0800

    Create 8.0-1.cpp

---
+++ C++程序设计/chapter8/8.0-1.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+class Student {
+	int num;
+	string name;
+	char sex;
+public:
+	void display() {
+		cout << "num: " << num << endl;
+		cout << "name: " << name << endl;
+		cout << "sex: " << sex << endl;
+	}
+};
+
+int main() {
+	
+	Student s1, s2;
+
+
+	return 0;
+}
