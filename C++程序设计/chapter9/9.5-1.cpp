bac01794852e0f3a25d156e367082fb213870897
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-07-30 11:00:40 +0800

    Create 9.5-1.cpp

---
+++ C++程序设计/chapter9/9.5-1.cpp
@@ -0,0 +1,39 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+class Student {
+public:
+	Student(int n,string nam,char s) {
+		num = n;
+		name = nam;
+		sex = s;
+		cout << "Constructor called." << endl;
+	}
+	
+	~Student() {
+		cout << "Destructor called." << num << endl;
+	}
+
+	void display() {
+		cout << "num: " << num << endl;
+		cout << "name: " << name << endl;
+		cout << "sex: " << sex << endl;
+	}
+
+private:
+	int num;
+	string name;
+	char sex;
+};
+
+int main() {
+	
+	Student stud1(10010, "Wang Li", 'f');
+	stud1.display();
+	Student stud2(10011, "Zhang Fun", 'm');
+	stud2.display();
+
+	return 0;
+}
