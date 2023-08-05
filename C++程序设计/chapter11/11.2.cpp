e167803b9b665b99e1c1a918d4eb61a23ab9c58a
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-08-05 15:39:30 +0800

    Create 11.2.cpp

---
+++ C++程序设计/chapter11/11.2.cpp
@@ -0,0 +1,40 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+class Student {
+public:
+	void get_value() {
+		cin >> num >> name >> sex;
+	}
+	void display() {
+		cout << "num: " << num << endl;
+		cout << "name: " << name << endl;
+		cout << "sex: " << sex << endl;
+	}
+private:
+	int num;
+	string name;
+	char sex;
+};
+
+class Student1:private Student {
+public:
+	void display_1() {
+		display();
+		cout << "age: " << age << endl;
+		cout << "address: " << addr << endl;
+	}
+private:
+	int age;
+	char addr[20];
+};
+
+int main(){
+
+	Student1 stud;
+	stud.display_1();
+
+	return 0;
+}
