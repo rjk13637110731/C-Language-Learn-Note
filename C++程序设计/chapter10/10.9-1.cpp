077298fbd1137479e257be0ff2438976c073d17c
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-08-05 10:56:05 +0800

    Create 10.9-1.cpp

---
+++ C++程序设计/chapter10/10.9-1.cpp
@@ -0,0 +1,61 @@
+# include <iostream>
+
+using namespace std;
+
+class Complex {
+public:
+	Complex() {
+		real = 0;
+		imag = 0;
+	}
+	Complex(double r,double i) {
+		real = r;
+		imag = i;
+	}
+	Complex operator+(Complex& c2);
+	operator double() {
+		return real;
+	}
+	friend ostream& operator<<(ostream&, Complex&);
+	friend istream& operator>>(istream&, Complex&);
+private:
+	double real;
+	double imag;
+};
+
+Complex Complex::operator+(Complex &c2) {
+	return Complex(real + c2.real, imag + c2.imag);
+}
+
+ostream& operator<<(ostream& output,Complex &c) {
+	output << "(" << c.real;
+	if (c.imag >= 0) {
+		output << "+";
+	} 
+	output << c.imag << "i)" << endl;
+	return output;
+}
+
+istream& operator>>(istream& input,Complex &c) {
+	cout << "input real part and imaginary part of complex number:";
+	input >> c.real >> c.imag;
+	return input;
+}
+
+int main() {
+	
+	Complex c1(2, 4), c2(6, 10), c3, c4, c5;
+	c3 = c1 + c2;
+	cout << c3;
+
+	cin >> c4 >> c5;
+	cout << "c4 = " << c4 << endl;
+	cout << "c5 = " << c5 << endl;
+
+	Complex a1(3, 4), a2(5, -10), a3;
+	double d;
+	d = 2.5 + a1;
+	cout << d << endl;
+
+	return 0;
+}
