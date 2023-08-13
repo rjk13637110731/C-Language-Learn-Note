fbb9f9040b928849fc04462d844bd2da3d56c761
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-08-13 22:21:59 +0800

    Add files via upload

---
+++ C++程序设计/chapter14/14.4/header2.h
@@ -0,0 +1,28 @@
+#pragma once
+# include <iostream>
+# include <string>
+# include <cmath>
+
+using namespace std;
+
+class Student {
+public:
+	Student(int n, string nam, char s) {
+		num = n;
+		name = name;
+		sex = s;
+	}
+	void get_data();
+private:
+	int num;
+	string name;
+	char sex;
+};
+
+void Student::get_data() {
+	cout << num << " " << name << " " << sex << endl;
+}
+
+double fun(double a, double b) {
+	return sqrt(a - b);
+}
