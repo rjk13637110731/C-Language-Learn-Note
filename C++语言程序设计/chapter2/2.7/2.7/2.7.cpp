2cd5869d36c2f735095e6678279440ebe05fc4b6
TangJ <fullmoonloong@163.com>
2023-08-14 19:00:24 +0800

    chapter2

---
+++ C++语言程序设计/chapter2/2.7/2.7/2.7.cpp
@@ -0,0 +1,17 @@
+//2_7.cpp
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int i = 0, sum = 0;
+	do {
+		sum += i;
+		i++;
+	} while (i<=10);
+
+	cout << "sum = " << sum << endl;
+
+	return 0;
+}
