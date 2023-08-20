381177cf3c7cff85b3dd4ce26d393f7182827865
TangJ <fullmoonloong@163.com>
2023-08-20 09:50:20 +0800

    chapter6

---
+++ C++语言程序设计/chapter6/6.5/6.5/6.5.cpp
@@ -0,0 +1,15 @@
+//6_5.cpp
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int i;
+	int* ptr = &i;
+	i = 10;
+	cout << "i = " << i << endl;
+	cout << "*ptr = " << *ptr << endl;
+
+	return 0;
+}
