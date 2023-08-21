174e34cff9389c91186c0396aad8a61174200000
TangJ <fullmoonloong@163.com>
2023-08-21 11:09:16 +0800

    chapter6

---
+++ C++语言程序设计/chapter6/6.12/6.12/6.12.cpp
@@ -0,0 +1,28 @@
+//6_12.cpp
+# include <iostream>
+
+using namespace std;
+
+class Point {
+public:
+	Point(int x = 0, int y = 0) :x(x), y(y) {}
+	int getX() const{
+		return x;
+	}
+
+	int getY() const{
+		return y;
+	}
+private:
+	int x, y;
+};
+
+int main() {
+	
+	Point a(4, 5);
+	Point* p1 = &a;
+	cout << p1->getX() << endl;
+	cout << p1->getY() << endl;
+
+	return 0;
+}
